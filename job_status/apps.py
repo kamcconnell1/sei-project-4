c889d461f5b2d46c602441764418aaded0aa3d3f
andy8radshaw <andy8radshaw@gmail.com>
2020-06-05 10:31:55 +0100

    added all required apps

---
+++ job_status/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class JobStatusConfig(AppConfig):
+    name = 'job_status'
---
+++ task_categories/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TaskCategoriesConfig(AppConfig):
+    name = 'task_categories'
