75f88f83946d0c5d87522294bb3878826fbab601
Katie <kamcconnell1@gmail.com>
2020-06-07 08:16:38 +0100

    fixed bug on login form

--- frontend/src/utils/useForm.js
+++ frontend/src/utils/useForm.js
@@ -2,18 +2,21 @@ import React from 'react'
 
 function useForm({ initialFormState = {}, submitFunction, submitParams = null, onSubmitSuccess = () => {} }) {
 
+  // * Initial state 
   const [formData, setFormData] = React.useState(initialFormState)
   const [formErrors, setFormErrors] = React.useState({})
 
 
-  //* HandleChange event for inputting values on form 
+  //* HandleChange event for inputting values on form & sets them and the errors to state 
   const handleChange = ({ target: { name, value } }) => {
     const updatedFormData = { ...formData, [name]: value }
     const updatedErrors = { ...formErrors, [name]: '' }
+  
     setFormData(updatedFormData)
     setFormErrors(updatedErrors)
   }
 
+  // * Handle submit function, on submitSuccess passed in as params 
   const handleSubmit = async event => {
     event.preventDefault()
     try {
