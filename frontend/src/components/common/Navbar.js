5821a61e48348c7df84b5a3d40ee0601d04e29ef
andy8radshaw <andy8radshaw@gmail.com>
2020-06-08 21:20:37 +0100

    finished navbar authentication

--- frontend/src/components/common/Navbar.js
+++ frontend/src/components/common/Navbar.js
@@ -30,48 +30,50 @@ class Navbar extends React.Component {
                 {isAuthenticated() &&
                   <>
                     <Link to='/jobs'>
-                      <Dropdown.Item>
+                      <Dropdown.Item id='navbar-btn1' >
                         Jobs
                       </Dropdown.Item>
                     </Link>
                     <Link to='/tasks'>
-                      <Dropdown.Item>
+                      <Dropdown.Item id='navbar-btn2'>
                         Tasks
                       </Dropdown.Item>
                     </Link>
                     <Link to='/contacts'>
-                      <Dropdown.Item>
+                      <Dropdown.Item id='navbar-btn3'>
                         Contacts
                       </Dropdown.Item>
                     </Link>
                     <Link to='/resources'>
-                      <Dropdown.Item>
+                      <Dropdown.Item id='navbar-btn4'>
                         Resources
                       </Dropdown.Item>
                     </Link>
                   </>}
                 <Dropdown.Divider />
-                <Dropdown.Header>Account</Dropdown.Header>
+                <Dropdown.Header id='navbar-account'>Account</Dropdown.Header>
                 {!isAuthenticated() &&
                   <>
                     <Link to='/login'>
-                      <Dropdown.Item>
+                      <Dropdown.Item id='navbar-btn5'>
                         Log In
                       </Dropdown.Item>
                     </Link>
                     <Link to='/register'>
-                      <Dropdown.Item>
+                      <Dropdown.Item id='navbar-btn6'>
                         Register
                       </Dropdown.Item>
                     </Link>
                   </>}
-                {isAuthenticated() && <Dropdown.Item onClick={this.handleLogout}>
-                      Log Out
+                {isAuthenticated() && <Dropdown.Item i onClick={this.handleLogout} >
+                  Log Out
                 </Dropdown.Item>}
               </Dropdown.Menu>
             </Dropdown>
             <Menu.Item id='navbar-logo' position='right'>
-              <Image size='tiny' src={logo} alt='jobr-logo' />
+              <Link to='/'>
+                <Image size='tiny' src={logo} alt='jobr-logo' />
+              </Link>
             </Menu.Item>
           </Container>
         </Menu>
