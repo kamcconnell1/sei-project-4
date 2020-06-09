26efbab8a069de67721cc8c8b314470178f53083
emmajphillips <emmjphillips@gmail.com>
2020-06-09 12:58:38 +0100

    reworking layout of job show page and job edit page

--- frontend/src/components/jobs/JobEdit.js
+++ frontend/src/components/jobs/JobEdit.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { useHistory, useParams } from 'react-router-dom'
-// import { DateInput } from 'semantic-ui-calendar-react'
 import { getSingleJob, editJob } from '../../lib/api'
+import { Grid, Header, Segment } from 'semantic-ui-react'
 import useFetch from '../../utils/useFetch'
 import useForm from '../../utils/useForm'
+
 import PageContainer from '../common/PageContainer'
-import FormWrapper from '../common/FormWrapper'
 import FormInput from '../common/FormInput'
 import FormButton from '../common/FormButton'
+
 function JobEdit() {
   const { id: jobId } = useParams()
   const history = useHistory()
@@ -15,6 +16,7 @@ function JobEdit() {
   const onSubmitSuccess = () => {
     history.push(`/jobs/${jobId}/`)
   }
+
   const { formData, handleChange, setFormData, formErrors, handleSubmit } = useForm({
     job_title: '',
     company: '',
@@ -29,157 +31,148 @@ function JobEdit() {
     country: '',
     status: null
   }, editJob, jobId, onSubmitSuccess)
+
   React.useEffect(() => {
     if (job) {
       setFormData({ ...job, status: job.status.id })
     }
   }, [job, setFormData])
+
   if (error) {
     console.log(error)
   }
+
   return (
     <PageContainer>
       {loading ?
-        'LOADING'
-        :
-        <FormWrapper
-          textAlign='center'
-          verticalAlign='middle'
-          formWidth='450'
-          color='orange'
-          onSubmit={handleSubmit}
-        >
-          <h1>{formData.job_title}</h1>
-          <div className='field'>
-            <label>Job title</label>
-            <FormInput
-              error={formErrors.job_title}
-              placeholder='Job title'
-              value={formData.job_title || ''}
-              type='text'
-              name='job_title'
-              onChange={handleChange}
-              className='fluid'
+        'LOADING' :
+        <Segment.Group>
+          <Segment textAlign='left'>
+            <Header as='h1' textAlign='center'>{job.job_title}</Header>
+            <Grid stackable columns={2}>
+              <Grid.Row stretched>
+                <Grid.Column width={12}>
+                  <Header size='small'>Job title</Header>
+                  <Segment>
+                    <p>{job.job_title}</p>
+                  </Segment>
+                </Grid.Column>
+                <Grid.Column width={4} only='computer tablet'>
+                  <Header size='small'>Application deadline</Header>
+                  <Segment>
+                    <p>{job.application_deadline ? job.application_deadline : 'No date'}</p>
+                  </Segment>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column width={12}>
+                  <Header size='small'>Company</Header>
+                  <Segment>
+                    <p>{job.company}</p>
+                  </Segment>
+                </Grid.Column>
+                <Grid.Column width={4} only='computer tablet'>
+                  <Header size='small'>Application submitted</Header>
+                  <Segment>
+                    <p>{job.application_submitted ? job.application_submitted : 'No date'}</p>
+                  </Segment>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row stretched>
+                <Grid.Column width={4}>
+                  <Header size='small'>City</Header>
+                  <Segment>
+                    <p>{job.city}</p>
+                  </Segment>
+                </Grid.Column>
+                <Grid.Column width={8}>
+                  <Header size='small'>Country</Header>
+                  <Segment>
+                    <p>{job.country}</p>
+                  </Segment>
+                </Grid.Column>
+                <Grid.Column width={4} only='computer tablet'>
+                  <Header size='small'>Interview date</Header>
+                  <Segment>
+                    <p>{job.interview_date ? job.interview_date : 'No date'}</p>
+                  </Segment>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row stretched>
+                <Grid.Column width={12}>
+                  <Header size='small'>URL</Header>
+                  <Segment>
+                    <p>{job.job_url}</p>
+                  </Segment>
+                </Grid.Column>
+                <Grid.Column width={4} only='computer tablet'>
+                  <Header size='small'>Job offer date</Header>
+                  <Segment>
+                    <p>{job.job_offer_date ? job.job_offer_date : 'No date'}</p>
+                  </Segment>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row stretched>
+                <Grid.Column width={6}>
+                  <Header size='small'>Salary</Header>
+                  <Segment>
+                    <p>{job.salary ? job.salary : 'No salary'}</p>
+                  </Segment>
+                </Grid.Column>
+                <Grid.Column width={6}>
+                  <Header size='small'>Status</Header>
+                  <Segment>
+                    <p>{job.status.name}</p>
+                  </Segment>
+                </Grid.Column>
+                <Grid.Column width={4} only='computer tablet'>
+                  <Header size='small'>Offer accepted</Header>
+                  <Segment>
+                    <p>{job.offer_accepted ? job.offer_accepted : 'No date'}</p>
+                  </Segment>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Column width={4} only='mobile'>
+                <Header size='small'>Application deadline</Header>
+                <Segment>
+                  <p>{job.application_deadline ? job.application_deadline : 'No date'}</p>
+                </Segment>
+              </Grid.Column>
+              <Grid.Column width={4} only='mobile'>
+                <Header size='small'>Application submitted</Header>
+                <Segment>
+                  <p>{job.application_submitted ? job.application_submitted : 'No date'}</p>
+                </Segment>
+              </Grid.Column>
+              <Grid.Column width={4} only='mobile'>
+                <Header size='small'>Interview date</Header>
+                <Segment>
+                  <p>{job.interview_date ? job.interview_date : 'No date'}</p>
+                </Segment>
+              </Grid.Column>
+              <Grid.Column width={4} only='mobile'>
+                <Header size='small'>Job offer date</Header>
+                <Segment>
+                  <p>{job.job_offer_date ? job.job_offer_date : 'No date'}</p>
+                </Segment>
+              </Grid.Column>
+              <Grid.Column width={4} only='mobile'>
+                <Header size='small'>Offer accepted</Header>
+                <Segment>
+                  <p>{job.offer_accepted ? job.offer_accepted : 'No date'}</p>
+                </Segment>
+              </Grid.Column>
+              <Grid.Column>
+              </Grid.Column>
+            </Grid>
+            <FormButton
+              fluidSize='large'
+              color='orange'
+              buttonText='Update'
+              type='submit'
             />
-          </div>
-          <div className='field'>
-            <label>Company</label>
-            <FormInput
-              error={formErrors.company}
-              placeholder='Company'
-              value={formData.company || ''}
-              type='text'
-              name='company'
-              onChange={handleChange}
-            />
-          </div>
-          {/* <div className='field'>
-            <label>Application deadline</label>
-            <DateInput
-              error={formErrors.application_deadline}
-              placeholder='Date'
-              value={formData.application_deadline || ''}
-              type='text'
-              name='application_deadline'
-              onChange={handleChange}
-            />
-          </div> */}
-          {/* 
-          <div className='field'>
-            <label>Application submitted</label>
-            <DateInput
-              placeholder='Date'
-              value={formData.application_submitted || ''}
-              type='date'
-              name='application_submitted'
-              onChange={handleChange}
-            />
-          </div>
-          <div className='field'>
-            <label>Interview date</label>
-            <FormInput
-              error={formErrors.interview_date}
-              placeholder='Date'
-              value={formData.interview_date || ''}
-              type='date'
-              name='interview_date'
-              onChange={handleChange}
-            />
-          </div>
-          <div className='field'>
-            <label>Offer accepted</label>
-            <FormInput
-              error={formErrors.offer_acceptance_date}
-              placeholder='Date'
-              value={formData.offer_acceptance_date || ''}
-              type='date'
-              name='offer_acceptance_date'
-              onChange={handleChange}
-            />
-          </div>
-          <div className='field'>
-            <label>Job URL</label>
-            <FormInput
-              error={formErrors.job_url}
-              placeholder='Enter link'
-              value={formData.job_url || ''}
-              type='text'
-              name='job_url'
-              onChange={handleChange}
-            />
-          </div>
-          <div className='field'>
-            <label>Salary</label>
-            <FormInput
-              error={formErrors.salary}
-              placeholder='Salary'
-              value={formData.salary || ''}
-              type='text'
-              name='salary'
-              onChange={handleChange}
-            />
-          </div>
-          <div className='field'>
-            <label>City</label>
-            <FormInput
-              error={formErrors.city}
-              placeholder='City'
-              value={formData.city || ''}
-              type='text'
-              name='city'
-              onChange={handleChange}
-            />
-          </div>
-          <div className='field'>
-            <label>Country</label>
-            <FormInput
-              error={formErrors.country}
-              placeholder='Country'
-              value={formData.country || ''}
-              type='text'
-              name='country'
-              onChange={handleChange}
-            />
-          </div>
-          <div className='field'>
-            <label>Status</label>
-            <select 
-              className='selection dropdown'
-              name='status'
-              value={formData.status.name}
-              onChange={handleChange}
-            >
-              <option value={formData.status.name}>{formData.status.name}</option>
-              <option value='Applied'>Applied</option>
-            </select>
-          </div> */}
-          <FormButton
-            fluidSize='large'
-            color='orange'
-            buttonText='Update'
-            type='submit'
-          ></FormButton>
-        </FormWrapper>
+          </Segment>
+        </Segment.Group>
       }
     </PageContainer >
   )
--- frontend/src/components/jobs/JobShow.js
+++ frontend/src/components/jobs/JobShow.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { getSingleJob } from '../../lib/api'
-import { Header, Grid, GridColumn, GridRow, Segment, Button } from 'semantic-ui-react'
+import { Header, Grid, Segment, Button } from 'semantic-ui-react'
 
 import PageContainer from '../common/PageContainer'
 
@@ -28,91 +28,121 @@ function JobShow() {
       <Segment.Group>
         <Segment textAlign='left'>
           <Header as='h1' textAlign='center'>{job.job_title}</Header>
-          <Grid stackable columns={3}>
-            <GridRow stretched>
-              <GridColumn width={12}>
+          <Grid stackable columns={2}>
+            <Grid.Row stretched>
+              <Grid.Column width={12}>
                 <Header size='small'>Job title</Header>
                 <Segment>
                   <p>{job.job_title}</p>
                 </Segment>
-              </GridColumn>
-              <GridColumn width={4}>
+              </Grid.Column>
+              <Grid.Column width={4} only='computer tablet'>
                 <Header size='small'>Application deadline</Header>
                 <Segment>
                   <p>{job.application_deadline ? job.application_deadline : 'No date'}</p>
                 </Segment>
-              </GridColumn>
-            </GridRow>
-            <GridRow>
-              <GridColumn width={12}>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column width={12}>
                 <Header size='small'>Company</Header>
                 <Segment>
                   <p>{job.company}</p>
                 </Segment>
-              </GridColumn>
-              <GridColumn width={4}>
+              </Grid.Column>
+              <Grid.Column width={4} only='computer tablet'>
                 <Header size='small'>Application submitted</Header>
                 <Segment>
                   <p>{job.application_submitted ? job.application_submitted : 'No date'}</p>
                 </Segment>
-              </GridColumn>
-            </GridRow>
-            <GridRow stretched>
-              <GridColumn width={4}>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row stretched>
+              <Grid.Column width={4}>
                 <Header size='small'>City</Header>
                 <Segment>
                   <p>{job.city}</p>
                 </Segment>
-              </GridColumn>
-              <GridColumn width={8}>
+              </Grid.Column>
+              <Grid.Column width={8}>
                 <Header size='small'>Country</Header>
                 <Segment>
                   <p>{job.country}</p>
                 </Segment>
-              </GridColumn>
-              <GridColumn width={4}>
+              </Grid.Column>
+              <Grid.Column width={4} only='computer tablet'>
                 <Header size='small'>Interview date</Header>
                 <Segment>
                   <p>{job.interview_date ? job.interview_date : 'No date'}</p>
                 </Segment>
-              </GridColumn>
-            </GridRow>
-            <GridRow stretched>
-              <GridColumn width={12}>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row stretched>
+              <Grid.Column width={12}>
                 <Header size='small'>URL</Header>
                 <Segment>
                   <p>{job.job_url}</p>
                 </Segment>
-              </GridColumn>
-              <GridColumn width={4}>
+              </Grid.Column>
+              <Grid.Column width={4} only='computer tablet'>
                 <Header size='small'>Job offer date</Header>
                 <Segment>
-                  <p>{job.job_offer_date ? job.application_submitted : 'No date'}</p>
+                  <p>{job.job_offer_date ? job.job_offer_date : 'No date'}</p>
                 </Segment>
-              </GridColumn>
-            </GridRow>
-            <GridRow stretched>
-              <GridColumn width={6}>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row stretched>
+              <Grid.Column width={6}>
                 <Header size='small'>Salary</Header>
                 <Segment>
-                  <p>{job.salary}</p>
+                  <p>{job.salary ? job.salary : 'No salary'}</p>
                 </Segment>
-              </GridColumn>
-              <GridColumn width={6}>
+              </Grid.Column>
+              <Grid.Column width={6}>
                 <Header size='small'>Status</Header>
                 <Segment>
                   <p>{job.status.name}</p>
                 </Segment>
-              </GridColumn>
-              <GridColumn width={4}>
+              </Grid.Column>
+              <Grid.Column width={4} only='computer tablet'>
                 <Header size='small'>Offer accepted</Header>
                 <Segment>
-                  <p>{job.offer_accepted ? job.application_submitted : 'No date'}</p>
+                  <p>{job.offer_accepted ? job.offer_accepted : 'No date'}</p>
                 </Segment>
-              </GridColumn>
-            </GridRow>
-            <GridColumn>
-            </GridColumn>
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Column width={4} only='mobile'>
+              <Header size='small'>Application deadline</Header>
+              <Segment>
+                <p>{job.application_deadline ? job.application_deadline : 'No date'}</p>
+              </Segment>
+            </Grid.Column>
+            <Grid.Column width={4} only='mobile'>
+              <Header size='small'>Application submitted</Header>
+              <Segment>
+                <p>{job.application_submitted ? job.application_submitted : 'No date'}</p>
+              </Segment>
+            </Grid.Column>
+            <Grid.Column width={4} only='mobile'>
+              <Header size='small'>Interview date</Header>
+              <Segment>
+                <p>{job.interview_date ? job.interview_date : 'No date'}</p>
+              </Segment>
+            </Grid.Column>
+            <Grid.Column width={4} only='mobile'>
+              <Header size='small'>Job offer date</Header>
+              <Segment>
+                <p>{job.job_offer_date ? job.job_offer_date : 'No date'}</p>
+              </Segment>
+            </Grid.Column>
+            <Grid.Column width={4} only='mobile'>
+              <Header size='small'>Offer accepted</Header>
+              <Segment>
+                <p>{job.offer_accepted ? job.offer_accepted : 'No date'}</p>
+              </Segment>
+            </Grid.Column>
+            <Grid.Column>
+            </Grid.Column>
           </Grid>
           <Link to={`/jobs/${job.id}/edit`}>
             <Button content='Update' className='button orange-button' fluid />
