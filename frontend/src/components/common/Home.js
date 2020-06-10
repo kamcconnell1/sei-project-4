54329e80d06951dc6797786859c319a72a5156f4
emmajphillips <emmjphillips@gmail.com>
2020-06-11 00:21:06 +0100

    initial home page content and styling

--- frontend/src/components/common/Home.js
+++ frontend/src/components/common/Home.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Header, Image } from 'semantic-ui-react'
+
+import PageContainer from './PageContainer'
+import exampleImage from '../../assets/jobr-example.png'
 
 const Home = () => {
 
   return (
-    <h1>Hello from JOBR</h1>
+    <PageContainer>
+      <div className='hero'>
+        <Header as='h1'>Take the work out of the job search</Header>
+        <Image src={exampleImage} size='massive'/>
+      </div>
+      <p>Finding a job can be a stressful and time-consuming process. JOBR helps make it just a bit easier by providing one platform to organise, prioritise and track your job search. Use JOBR to create your own job board to store and update applications as they progress through various stages, from your initial search to when you finally land the offer.</p>
+    </PageContainer>
   )
 }
 
