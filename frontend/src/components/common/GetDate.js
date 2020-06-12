b14df1115ae0e800de1fb9a824a1eb509b314dd3
Katie <kamcconnell1@gmail.com>
2020-06-12 12:53:50 +0100

    fixed date function

--- frontend/src/components/common/GetDate.js
+++ frontend/src/components/common/GetDate.js
@@ -4,18 +4,20 @@ import React from 'react'
 
 function GetDate(date) {
 
-  const today_date = new Date().getTime()
+  const today_date = new Date()
+  const formatted_today_date = (new Date(today_date.getTime() - (today_date.getTimezoneOffset() * 60000))).toISOString().split('T')[0]
+
+  const today_date_with_time = new Date().getTime()
   const formatted_date = (new Date(date).getTime())
-  const microSecondsDiff = Math.abs(formatted_date - today_date)
+  const microSecondsDiff = Math.abs(formatted_date - today_date_with_time)
   
-  // ! NEED TO UPDATE THIS ONE SO HAVE DATES IN SAME FORMAT TO COMPARE
-  if (date === today_date) 
+  if (date === formatted_today_date) 
     return <p className='due-today'>0 days</p>
 
-  if (formatted_date > today_date) 
+  if (formatted_date > today_date_with_time) 
     return <p className='due'>{Math.floor(microSecondsDiff / (1000 * 60 * 60 * 24))} days</p>
   
-  if (formatted_date < today_date) 
+  if (formatted_date < today_date_with_time) 
     return <p className='overdue'>{Math.floor(microSecondsDiff / (1000 * 60 * 60 * 24))} days</p>
 
 }
